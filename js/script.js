2d2eb1559ed551011a4a110e6811b9e8f92c916e
Chathura Sankalpa <87167914+csmaxsi@users.noreply.github.com>
2023-06-19 09:59:45 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height ) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+ScrollReveal({
+    reset: true,  
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwlp_ZJnyrwg8TDWI9O81G51Xlf2X9lKRbTWOWJTW1VnBnPSd7Ggfw5oHEVOpcJ3IXr/exec'
+  const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
